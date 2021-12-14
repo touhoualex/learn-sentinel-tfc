ca6e81e192b224f8b2c4126fd00f32c3f77f0189
alex <95730915+touhoualex@users.noreply.github.com>
2021-12-15 00:47:10 +0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,10 +13,10 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "tfc-iac-org"
 
     workspaces {
-      name = "sentinel-example"
+      name = "learn-sentinel-tfc"
     }
   }
 
